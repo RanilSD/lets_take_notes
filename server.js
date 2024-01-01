9a91cea23e8d318e135f182d629feb2cbc2e59de
Ra <ranilsd86@gmail.com>
2024-01-01 15:45:01 -0500

    added routes folder with files to work in as well as the server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//initializing app and creating port
+
+const app = express();
+const PORT = process.env.Port || 3001;
+
+//setting up the middleware for routes and all css, and javaScript from public folder files
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//starting the server on port
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
